c59407379ba5525602ffc2e286fbb07648506d6f
shell chen <shell_chen@yeah.net>
2015-08-04 18:00:56 +0800

    fix bug

--- plugin/blog.py
+++ plugin/blog.py
@@ -8,7 +8,7 @@
 import os
 import time
 import json
-
+import logging
 from flask import render_template, request
 
 from sunshine import ClassViews
@@ -83,9 +83,10 @@ class Chapters(ClassViews):
         Id = request.args.get("id")
         data = json.loads(request.data).get("tex","")
         dir_path = os.path.join(STOREPATH, str(Id))
-
+	
         if not os.path.isdir(dir_path):
-            return "-1"   #路径不存在
+            logging.info("%s: is not exit"%dir_path)
+	    return "-1"   #路径不存在
         file_path = os.path.join(dir_path, "index.md")
 
         with open(file_path,"w") as fp:
--- setting.py
+++ setting.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 import os
 curdir = os.path.dirname(os.path.realpath(__file__))
-home = os.environ.get("HOME")
-STOREPATH = os.path.join(home, "/shell/blog/store")
+STOREPATH = os.path.join( os.environ.get("HOME","/home/cxx/"),  "shell/blog/store")
